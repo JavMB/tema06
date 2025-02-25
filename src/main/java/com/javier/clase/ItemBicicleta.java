e7ff6cec45018a5d1069b7f92891807212a1f75f
Javi <javiermengual@live.com>
2025-02-25 11:21:23 +0100

    add

---
+++ src/main/java/com/javier/clase/ItemBicicleta.java
@@ -0,0 +1,36 @@
+package com.javier.clase;
+
+public class ItemBicicleta {
+    private Bicicleta bicicleta;
+    private int stock;
+
+    public ItemBicicleta(Bicicleta bicicleta, int stock) {
+        this.bicicleta = bicicleta;
+        this.stock = stock;
+    }
+
+    public Bicicleta getBicicleta() {
+        return bicicleta;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void incrementarStock() {
+        stock++;
+    }
+
+    public void decrementarStock() {
+        if(stock > 0) {
+            stock--;
+        } else {
+            throw new IllegalStateException("Stock insuficiente");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return bicicleta.toString() + " Stock: " + stock;
+    }
+}
